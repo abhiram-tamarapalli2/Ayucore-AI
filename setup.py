dbe83ea3782f846a3d3410c31ea311eb4318e832
abhiram <abhiramtamarapalli2@gmail.com>
2025-07-23 17:23:51 +0530

    phase 1

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Ayucore-AI',
+    version= '0.0.0',
+    author= 'Abhiram Tamarapalli',
+    author_email= 'abhiramtamarapalli2@gmail.com',
+    packages= find_packages(),
+    install_requires = [],
+
+)
